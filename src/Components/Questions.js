44f633f327d99f1b914811b0fc6ac9174a22800b
Lauren Bullen <laurenbullen@comcast.net>
2019-12-04 22:44:00 -0800

    added question component

---
+++ src/Components/Questions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Question = props => {
+    return (
+        <h2 className='question'>{props.content}</h2>
+    );
+}
+
+Question.PropTypes = {
+    content = PropTypes.string.isRequired
+};
+
+export default Question
