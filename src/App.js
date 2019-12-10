a6ce6c2bba0db39b571a6e40235b6ca466b183e0
Lauren Bullen <laurenbullen@comcast.net>
2019-12-09 20:17:41 -0800

    question counter component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from './Components/Question';
+import QuestionCount from './Components/QuestionCount'
 
-function App() {
-  return (
+class App extends React.Component {
+  render (){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>React Quiz</h2>
       </header>
+      <Question content='What is your favorite holiday?' />
+      <QuestionCount />
     </div>
   );
 }
+};
 
 export default App;
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const Question = props => {
+const Question =(props)=> {
     return (
         <h2 className='question'>{props.content}</h2>
     );
 }
 
-Question.PropTypes = {
-    content = PropTypes.string.isRequired
+Question.propTypes = {
+    content: PropTypes.string.isRequired
 };
 
-export default Question
+export default Question;
